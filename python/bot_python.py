99b5f2a9e57ddb9dcb7c1e0109fbf28ce837d3d2
mack hyuuga <mackhyuuga@hotmail.com>
2020-06-05 19:34:50 -0300

    ...

--- python/bot_python.py
+++ python/bot_python.py
@@ -3,14 +3,14 @@ from time import sleep
 
 class TinderBot():
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
 
     def login(self):
         self.driver.get('https://tinder.com/?lang=pt')
 
         sleep(30)
         
-        for i in range(1, 1000):
+        for i in range(1, 10000):
             sleep(0.5)
             like = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
             like.click()
